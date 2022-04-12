9ad9d84b2f8fb2837ac73a0a4dad2bbc65907e3f
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-13 00:19:20 +0100

    event listener on pressed keys

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Hello!");
+// alert("Hello!");
+
+//vanilla
+//var input = document.getElementById('user-input')
+//jquery
+var input = $('user-input').val(); //val returns value of what is referenced.
+
+
+$(document).ready(function(){
+//function between brackets executes only when ready.
+//get value of input, event listener
+    $(document).keypress(function(){
+        alert("hello")
+    })
+})
+
