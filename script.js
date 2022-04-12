8d443d460950cabf217da05681ea31093b1f1e14
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-13 00:35:13 +0100

    textbox returns entered text

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 
 //vanilla
 //var input = document.getElementById('user-input')
-//jquery
-var input = $('user-input').val(); //val returns value of what is referenced.
+
 
 
 $(document).ready(function(){
 //function between brackets executes only when ready.
 //get value of input, event listener
     $(document).keypress(function(key){
-        alert(key.which);
+        if(key.which === 13 && $('#user-input').is(':focus')) { 
+        //if key is enter and text box selected.
+        //jquery
+        var value = $('#user-input').val(); //val returns value of what is referenced.
+        alert(value);
+        }
     })
 })
 
