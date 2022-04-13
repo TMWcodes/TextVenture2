37e0cc4729ae42811b5bdeb5181a64b142895387
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-13 16:28:05 +0100

    several rooms with descriptions

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 //vanilla
 //var input = document.getElementById('user-input')
 
+var rooms = {
+    "start": {
+        "description": "<p>This it the description for the start</p>"
+    },
+    "room 1": {
+        "description": "<p>This it the description for room1</p>"
+    },
+    "room 2" :{
+        "description": "<p>This it the description for room2</p>"
+    }
+    }
+    
 
 
 $(document).ready(function(){
+    $('#game-text').append("<p>" + rooms["start"].description + "</p>");
 //function between brackets executes only when ready.
 //get value of input, event listener
     $(document).keypress(function(key){
