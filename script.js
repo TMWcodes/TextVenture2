277b2daf1a61f42ce59c62994f50838db8beebd5
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-04-20 17:17:57 +0100

    refactored directions and added alert for unavailable actions

--- script.js
+++ script.js
@@ -8,13 +8,40 @@
 //keeping track of the current room
 var currentRoom = "start";
 
+//if room, currentRoom (start) direction "exists" (is not undefined)
+//then current room will be updated. 
 function changeRoom(dir) {
     if(rooms[currentRoom].directions[dir] !== undefined) {
         currentRoom = rooms[currentRoom].directions[dir];
         $('#game-text').append("<p>" + rooms[currentRoom].description + "</p>");
+        //if the move is valid but the direction is not available (in game) state you cannot go that way
+    } else {
+        $('#game-text').append("<p>you cannot go that way</p>");
     }
 }
 
+function playerInput(input) {
+    // separate words by space
+    var command = input.split(" ")[0];
+//switch case
+switch(command) {
+    case "go":
+        var dir = input.split(" ")[1];
+        changeRoom(dir)
+        break;
+    // case "south":
+    //     changeRoom("south")
+    //     break;
+    // case "east":
+    //     changeRoom("east")
+    //     break;
+    // case "west":
+    //     changeRoom("west")
+    //     break;
+    default:
+        alert(" Invalid command");
+}
+}
 //function between brackets executes only when ready.
 $(document).ready(function(){
 
@@ -27,23 +54,8 @@ $(document).ready(function(){
       
         //jquery
             var value = $('#user-input').val().toLowerCase(); //user input to lowercase val returns value of what is referenced.
-            //switch case
-            switch(value) {
-                case "north":
-                    changeRoom("north")
-                    break;
-                case "south":
-                    changeRoom("south")
-                    break;
-                case "east":
-                    changeRoom("east")
-                    break;
-                case "west":
-                    changeRoom("west")
-                    break;
-                default:
-                    alert("invalid move");
-            }
+            playerInput(value);
+            
 
         }
     })
